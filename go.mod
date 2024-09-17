f2d6116d88abe4e44dac346dab901f6da675e6ee
Vivek <vs690799@gmail.com>
2024-09-18 02:04:11 +0530

    added mux router

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vivek700/goWeb
 
 go 1.23.0
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -4,20 +4,34 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
+
+	r := mux.NewRouter()
+
 	fmt.Println("Server starting...")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to my website")
 		// fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
 
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
 
-	fmt.Println("Listening on :8080...")
-	err := http.ListenAndServe(":8080", nil)
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	}).Methods("GET")
+
+	// fs := http.FileServer(http.Dir("static/"))
+
+	// http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	fmt.Println("Listening on: 8080...")
+	err := http.ListenAndServe(":8080", r)
 
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
